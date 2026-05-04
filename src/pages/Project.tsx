8e809b4a78e6be8253d91d10d6813173da5c712a
fadilarazhya <fadilarazhya03@gmail.com>
2026-05-04 09:32:38 +0700

    Tools clear

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -13,7 +13,7 @@ const Project = () => {
       id="project"
       className="px-8 py-8 bg-white md:py-16 dark:bg-gray-900 antialiased">
       <div className="max-w-screen-xl px-4 mx-auto 2xl:px-0">
-        <div className="py-8 lg:px-12 lg:py-16">
+        <div className="py-8 lg:px-10 lg:py-16">
           <h1 className="mb-6 text-4xl font-semibold leading-none tracking-tight text-gray-900 dark:text-white lg:text-5xl">
             Project
           </h1>
@@ -72,45 +72,6 @@ const Project = () => {
               copywriting.
             </p>
             <div className="flex space-y-8 space-x-4">
-              {/*Figma Start */}
-              <div
-                data-tooltip-target="tooltip-top-figma"
-                data-tooltip-placement="top">
-                <svg
-                  className="w-6 h-6 sm:w-8 sm:h-8"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 128 128">
-                  <path
-                    fill="#0acf83"
-                    d="M45.5 129c11.9 0 21.5-9.6 21.5-21.5V86H45.5C33.6 86 24 95.6 24 107.5S33.6 129 45.5 129zm0 0"
-                  />
-                  <path
-                    fill="#a259ff"
-                    d="M24 64.5C24 52.6 33.6 43 45.5 43H67v43H45.5C33.6 86 24 76.4 24 64.5zm0 0"
-                  />
-                  <path
-                    fill="#f24e1e"
-                    d="M24 21.5C24 9.6 33.6 0 45.5 0H67v43H45.5C33.6 43 24 33.4 24 21.5zm0 0"
-                  />
-                  <path
-                    fill="#ff7262"
-                    d="M67 0h21.5C100.4 0 110 9.6 110 21.5S100.4 43 88.5 43H67zm0 0"
-                  />
-                  <path
-                    fill="#1abcfe"
-                    d="M110 64.5c0 11.9-9.6 21.5-21.5 21.5S67 76.4 67 64.5 76.6 43 88.5 43 110 52.6 110 64.5zm0 0"
-                  />
-                </svg>
-              </div>
-              <div
-                id="tooltip-top-figma"
-                role="tooltip"
-                className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-xs opacity-0 tooltip dark:bg-gray-700">
-                Figma
-                <div className="tooltip-arrow" data-popper-arrow></div>
-              </div>
-              {/*Figma End */}
-
               {/*React Start */}
               <div
                 data-tooltip-target="tooltip-top-react"
